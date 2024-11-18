a8d47e3375c1f093f83e7b6573f169e5b4253e6b
mephistopheles-ilya <ilyuxa.popoff@yandex.ru>
2024-11-19 02:29:17 +0300

    added time

--- final_test.sh
+++ final_test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+echo "Get projections"
 
-./get --projections=400 --init_file=InitialModels/InitialModel5 --directory=init_examples1
+time ./get --projections=400 --init_file=InitialModels/InitialModel5 --directory=init_examples1
 
-./get --projections=400 --init_file=InitialModels/InitialModel5 --directory=init_examples2 --shift_poly3d=equations
+time ./get --projections=400 --init_file=InitialModels/InitialModel5 --directory=init_examples2 --shift_poly3d=equations
 
-./tran --directory_in=init_examples1 --directory_out=res_examples1 --projection=400 --shift=0 --points_in_contour=2000
+echo "Spoil projections"
 
-./tran --directory_in=init_examples2 --directory_out=res_examples2 --projection=400 --shift=1 --points_in_contour=2000
+time ./tran --directory_in=init_examples1 --directory_out=res_examples1 --projection=400 --shift=0 --points_in_contour=2000
 
-./sd init_examples1 init_examples2 400 > sym_diff_compare.txt
+time ./tran --directory_in=init_examples2 --directory_out=res_examples2 --projection=400 --shift=1 --points_in_contour=2000
 
-./gc res_examples1 res_examples2 400 > gorizontal_compare.txt
+echo "Compare prijections"
+
+time ./sd init_examples1 init_examples2 400 > sym_diff_compare.txt
+
+time ./gc res_examples1 res_examples2 400 > gorizontal_compare.txt
 
